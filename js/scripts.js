745870d8f842908f4f43206db0d12aafbf157228
Lindsay <lindsaygrizzard@gmail.com>
2018-08-05 16:17:38 -0700

    Placeholder background colors added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
+var colors = ['#CCF5AC', '#BAD29F', '#808A9F', '#2C497F', '#3D2B56'];
+
 var word = words[Math.floor(Math.random()*words.length)];
+var color = colors[Math.floor(Math.random()*colors.length)];
 
 var wordName = word.word;
 var wordType = word.wordtype;
@@ -9,4 +12,7 @@ $( '.js-word' ).html( wordName );
 $( '.js-type' ).html( wordType );
 $( '.js-definition' ).html( wordDefinition);
 $( '.js-synonyms' ).html( wordSynonyms );
-$( '.js-dict-link' ).attr("href", "https://www.merriam-webster.com/dictionary/"+ wordName );
+$( '.js-dict-link' ).attr("href", "https://www.merriam-webster.com/dictionary/" + wordName );
+
+$( 'body' ).css("background-color", color);
+
