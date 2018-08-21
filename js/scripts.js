6d07f9a327a5f21a450d556ba32a107558801e35
Lindsay <lindsaygrizzard@gmail.com>
2018-08-21 07:51:13 -0700

    background js working with local storage iterating over array

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,40 @@
-var colors = ['#5F0B0D', '#5E2136', '#4F325C', '#214187', '#253764', '#1F2942', '#0F151F', '#132521', '#16321F', '#183F1D'];
+// Randomly select words from object and place into DOM
+var $word = words[Math.floor(Math.random()*words.length)];
 
-var word = words[Math.floor(Math.random()*words.length)];
-var color = colors[Math.floor(Math.random()*colors.length)];
+var $wordName = $word.word;
+var $wordType = $word.wordtype;
+var $wordDefinition = $word.definition;
+var $wordSynonyms = $word.synonyms;
 
-var wordName = word.word;
-var wordType = word.wordtype;
-var wordDefinition = word.definition;
-var wordSynonyms = word.synonyms;
+$( '.js-word' ).html( $wordName );
+$( '.js-type' ).html( $wordType );
+$( '.js-definition' ).html( $wordDefinition);
+$( '.js-synonyms' ).html( $wordSynonyms );
+$( '.js-dict-link' ).attr('href', 'https://www.merriam-webster.com/dictionary/' + $wordName );
 
-$( '.js-word' ).html( wordName );
-$( '.js-type' ).html( wordType );
-$( '.js-definition' ).html( wordDefinition);
-$( '.js-synonyms' ).html( wordSynonyms );
-$( '.js-dict-link' ).attr("href", "https://www.merriam-webster.com/dictionary/" + wordName );
 
-$( 'body' ).css("background-color", color);
+// modify array
+function nextVal(arr) {
+	firstElm = arr.shift();
+	arr.push(firstElm);
+	backgroundColorDelta(firstElm);
+	return arr;
+}
 
+// set background color
+function backgroundColorDelta (firstElm) {
+	$( 'body' ).css('background-color', firstElm);
+}
+
+if (localStorage.getItem('localColorArr') === null) {
+	//set array first time if it doesn't exist
+	var colorsArr = ['#5F0B0D', '#5E2136', '#4F325C', '#214187', '#253764', '#1F2942', '#0F151F', '#132521', '#16321F', '#183F1D'];
+	localStorage.setItem('localColorArr', JSON.stringify(colorsArr));
+	var localColorArr = JSON.parse(localStorage.getItem('localColorArr'));
+} else {
+	var localColorArr = JSON.parse(localStorage.getItem('localColorArr'));
+	//modify array
+	modifiedColors = nextVal(localColorArr);
+	// set setLocalStorage = modifiedArray;
+	localStorage.setItem('localColorArr', JSON.stringify(modifiedColors));
+}
